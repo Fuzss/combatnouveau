f46924256df9f3b167563e912bbef236afb0fbce
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-12-18 08:49:28 +0100

    upgrade 1.21.11 workspace

---
+++ 1.21.11/Common/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("fuzs.multiloader.multiloader-convention-plugins-common")
+}
+
+dependencies {
+    modCompileOnlyApi(libs.puzzleslib.common)
+}
+
+multiloader {
+    mixins {
+        mixin("EntityMixin", "FoodDataMixin", "PlayerMixin", "ProjectileUtilMixin")
+        clientMixin("ItemAttributeModifiers\$Display\$DefaultMixin", "LivingEntityMixin", "MinecraftMixin")
+        clientAccessor("MultiPlayerGameModeAccessor")
+    }
+}
